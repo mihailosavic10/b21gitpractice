528609f3b28f42fdf08fda02d6c2b257af5845d0
mihailosavic10 <mihailo.savic@gmail.com>
2021-01-27 23:02:02 -0500

    watched movie for 20 min more

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("Hello World, b22");
         System.out.println("B21 Future SDET");
         //movie on the phone
+        //watched movie for 20 min more
     }
 }
