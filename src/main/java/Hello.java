dfc053d22249c0e4990b21e4c1a300bee1ebd4fb
mihailosavic10 <mihailo.savic@gmail.com>
2021-01-27 22:50:09 -0500

    master branch added some text

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello World, b22");
         System.out.println("B21 Future SDET");
     }
 }
