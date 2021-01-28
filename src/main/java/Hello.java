35aaaf8d041c9ae9cc32af99a4026b69c25dbfa2
mihailosavic10 <mihailo.savic@gmail.com>
2021-01-27 22:53:44 -0500

    final work on movie 1 on the master

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("Hello World, b21");
         System.out.println("B21 Future SDET");
         //watching more movies
+        //reDY TO GO back
     }
 }
