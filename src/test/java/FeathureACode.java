58946a90e1fb9d5c7507d59df8fae6d05df1e4a3
mihailosavic10 <mihailo.savic@gmail.com>
2021-01-27 21:52:30 -0500

    more working on branch feathure a

--- src/test/java/FeathureACode.java
+++ src/test/java/FeathureACode.java
@@ -3,5 +3,6 @@ public class FeathureACode {
     public static void main(String[] args) {
 
         System.out.println("Awesome work startet!!!");
+        System.out.println("B21 is learning git");
     }
 }
